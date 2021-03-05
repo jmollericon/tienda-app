bb00b8a56893ecccf8aa6849ead5e8794e4c8b0f
Jorge Mollericon <jlmollericon@gmail.com>
2021-03-05 16:21:21 -0400

    Add middleware auth (routes>web.php), add auth scafolding, for laravel 8.* use: composer require laravel/ui, php artisan ui vue --auth, npm install, npm run dev

--- routes/web.php
+++ routes/web.php
@@ -15,38 +15,44 @@ use App\Models\Product;
 |
 */
 
-Route::get('/products', function () {
-    $products = Product::orderBy('created_at', 'desc')->get(); # all();
-    return view('products.index', compact('products'));
-})->name('products.index');
+Route::middleware('auth')->group(function () {
+    Route::get('/products', function () {
+        $products = Product::orderBy('created_at', 'desc')->get(); # all();
+        return view('products.index', compact('products'));
+    })->name('products.index');
 
-Route::get('/products/create', function () {
-    return view('products.create');
-})->name('products.create');
+    Route::get('/products/create', function () {
+        return view('products.create');
+    })->name('products.create');
 
-Route::post('/products', function (Request $request) {
-    $new_product = new Product;
-    $new_product->description = $request->input('description');
-    $new_product->price = $request->input('price');
-    $new_product->save();
-    return redirect()->route('products.index')->with('info', 'Product created successfully');
-})->name('products.store');
+    Route::post('/products', function (Request $request) {
+        $new_product = new Product;
+        $new_product->description = $request->input('description');
+        $new_product->price = $request->input('price');
+        $new_product->save();
+        return redirect()->route('products.index')->with('info', 'Product created successfully');
+    })->name('products.store');
 
-Route::delete('/products/{id}', function ($id) {
-    $product = Product::findOrFail($id);
-    $product->delete();
-    return redirect()->route('products.index')->with('info', 'Product deleted successfully');
-})->name('products.destroy');
+    Route::delete('/products/{id}', function ($id) {
+        $product = Product::findOrFail($id);
+        $product->delete();
+        return redirect()->route('products.index')->with('info', 'Product deleted successfully');
+    })->name('products.destroy');
 
-Route::get('/products/{id}/edit', function ($id) {
-    $product = Product::find($id);
-    return view('products.edit', compact('product'));
-})->name('products.edit');
+    Route::get('/products/{id}/edit', function ($id) {
+        $product = Product::find($id);
+        return view('products.edit', compact('product'));
+    })->name('products.edit');
 
-Route::put('/products/{id}', function (Request $req, $id) {
-    $product = Product::findOrFail($id);
-    $product->description = $req->input('description');
-    $product->price = $req->input('price');
-    $product->save();
-    return redirect()->route('products.index')->with('info', 'Product updated successfully');
-})->name('products.update');
+    Route::put('/products/{id}', function (Request $req, $id) {
+        $product = Product::findOrFail($id);
+        $product->description = $req->input('description');
+        $product->price = $req->input('price');
+        $product->save();
+        return redirect()->route('products.index')->with('info', 'Product updated successfully');
+    })->name('products.update');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
